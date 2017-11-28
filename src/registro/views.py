5bed9cd8b4f1f82be70c492970abf49adf2eb3a1
Angelita7 <angelita_r7@hotmail.com>
2017-11-27 20:13:28 -0500

    Proyecto Lab.Ingesoft

---
+++ src/registro/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from.forms import RegistroForm
+from.models import Registro
+from django.views.generic import ListView, TemplateView
+
+# Create your views here.
+
+def indexRegistro(request):
+	return render(request, 'inicio/index.html')
+
+
+def registro_view(request):
+	if request.method == 'POST':
+		form = RegistroForm(request.POST)
+		if form.is_valid():
+			form.save()
+		#return redirect('compra:index_compra')
+	else:
+		form = RegistroForm()
+	return render(request, 'registro/registro_form.html', {'form':form})
+
+
+class RegistroList(ListView):
+	model = Registro
+	template_name = 'registro/registro_list.html'
